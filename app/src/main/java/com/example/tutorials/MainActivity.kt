a9cf917df02e60d076cbaeec19cfb3ed6592b616
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-30 15:49:08 +0500

    add layout basics and practice linear layout by building basic app

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.tutorials
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,31 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val list = listOf(5, 3, 6, 7, 9, 1)
-        sortedList(list)
-//        println(list)
-        Log.d("MainActivity", "Hello, this is our first log message.")
-    }
-
-    private fun sortedList(list: List<Int>) {
-        for(i in 0..list.size - 1) {
-            for(j in 0..list.size - 2) {
-                if(list[j] > list[j+1]) {
-                    Collections.swap(list, j, j+1)
-                }
-            }
-        }
-    }
-
-    //activity lifecycle methods
-    override fun onPause() {
-        super.onPause()
-        println("onPause")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        println("onRestart")
     }
 }
