c41680f22e0b7a6a1b2cb1bc3d3404dccf25c478
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2025-01-01 11:22:24 +0500

    working with buttons in Android

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.tutorials
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+//        val btnApply = findViewById<Button>(R.id.btnApply)
+
+        btnApply.setOnClickListener {
+            val firstName = edtFirstName.text.toString()
+            val lastName = edtLastName.text.toString()
+            val birthDate = edtDateOfBirth.text.toString()
+            val country = edtCountry.text.toString()
+            Log.d("MainActivity", "$firstName $lastName born on $birthDate from $country just applied to formular.")
+
         }
     }
 }
