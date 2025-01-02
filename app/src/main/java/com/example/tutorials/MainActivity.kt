1b4a8afebb3a5a491a717e60a544abcc52cb01d5
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2025-01-02 16:03:44 +0500

    add code of working with checkboxes and radio buttons by building basic app

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.tutorials
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,17 +25,33 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val edFirstNumber = findViewById<EditText>(R.id.etFirstNumber)
-        val edSecondNumber = findViewById<EditText>(R.id.etSecondNumber)
-        val tvResult = findViewById<TextView>(R.id.tvResult)
-        val  btnAdd = findViewById<Button>(R.id.btnAdd)
 
-        btnAdd.setOnClickListener {
-            val firstNumber = edFirstNumber.text.toString().toInt()
-            val serviceConfig = edSecondNumber.text.toString().toInt()
-            val result = firstNumber + serviceConfig
-            tvResult.text = result.toString()
+        val rgMeat = findViewById<RadioGroup>(R.id.rgMeat)
+        val rbBeef = findViewById<RadioButton>(R.id.rbBeef)
+        val rbCheese = findViewById<RadioButton>(R.id.rbBeef)
+        val rbPork = findViewById<RadioButton>(R.id.rbPork)
+
+        val cbCheese = findViewById<CheckBox>(R.id.cbCheese)
+        val cbOnions = findViewById<CheckBox>(R.id.cbOnions)
+        val cbSalad = findViewById<CheckBox>(R.id.cbSalad)
+
+        val tvOrder = findViewById<TextView>(R.id.tvOrder)
+
+        val btnOrder = findViewById<Button>(R.id.btnOrder)
+
+        btnOrder.setOnClickListener{
+            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+            val cheese = cbCheese.isChecked
+            val onions = cbOnions.isChecked
+            val salad = cbSalad.isChecked
+
+            val orderString = "You Ordered a burger with: \n" +
+                    "${meat.text}" +
+                    (if(cheese) "\n- Cheese" else "") +
+                    (if(onions) "\n- Onions" else "") +
+                    (if(salad) "\n- Onions" else "")
+            tvOrder.text = orderString
         }
-
     }
 }
