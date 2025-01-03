8b9f1ea1179703163ba20259a687613d03aeae13
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2025-01-03 08:12:35 +0500

    passing name:value data between activities using Intents

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.tutorials
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +22,21 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val btnOpenActivity = findViewById<Button>(R.id.btnOpenActivity)
-        btnOpenActivity.setOnClickListener {
-            Intent(this, SecondActivity::class.java).also {
+        val etName = findViewById<TextView>(R.id.etName)
+        val etAge = findViewById<TextView>(R.id.etAge)
+        val etCountry = findViewById<TextView>(R.id.etCountry)
+
+        val btnApply = findViewById<Button>(R.id.btnApply)
+        btnApply.setOnClickListener {
+
+            val name = etName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val country = etCountry.text.toString()
+
+            Intent(this, SecondActivity2::class.java).also {
+                it.putExtra("EXTRA_NAME", name)
+                it.putExtra("EXTRA_AGE", age)
+                it.putExtra("EXTRA_COUNTRY", country)
                 startActivity(it)
             }
         }
---
+++ app/src/main/java/com/example/tutorials/SecondActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.tutorials
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val name = intent.getStringExtra("EXTRA_NAME")
+        val age = intent.getIntExtra("EXTRA_AGE", 0)
+        val country = intent.getStringExtra("EXTRA_COUNTRY")
+
+        val personString = "$name is $age years old and lives in $country"
+
+        val tvPerson = findViewById<TextView>(R.id.tvPerson)
+        tvPerson.text = personString
+    }
+}
