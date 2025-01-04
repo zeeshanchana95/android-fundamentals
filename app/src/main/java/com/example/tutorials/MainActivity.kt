6d6a882ccfef0c5a94f4106d463ae868a537d33e
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2025-01-04 22:06:25 +0500

    add spinner in app

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tutorials
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
@@ -20,61 +24,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val btnDialog1 = findViewById<Button>(R.id.btnDialog1)
-        val btnDialog2 = findViewById<Button>(R.id.btnDialog2)
-        val btnDialog3 = findViewById<Button>(R.id.btnDialog3)
+        val spMonths = findViewById<Spinner>(R.id.spMonths)
 
-        val addContactDialog = AlertDialog.Builder(this)
-            .setTitle("Add Contact")
-            .setMessage("Do you want to add Mr. Poop to your contacts list.")
-            .setIcon(R.drawable.ic_add_contact)
-            .setPositiveButton("Yes") { _, _ ->
-                Toast.makeText(this, "You added Mr. Poop to your contact list.", Toast.LENGTH_SHORT).show()
+        val customList = listOf("First", "Second", "Third", "Fourth")
+        val adapter = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, customList)
+        spMonths.adapter = adapter
+
+        spMonths.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MainActivity,
+                    "You selected ${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_LONG).show()
             }
-            .setNegativeButton("No") { _, _ ->
-                Toast.makeText(this, "You didn't add Mr. Poop to your contact list.", Toast.LENGTH_SHORT).show()
-            }.create()
 
-        btnDialog1.setOnClickListener {
-            addContactDialog.show()
-        }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
 
-        val options = arrayOf("First Item", "Second Item", "Third Item")
-        val singleChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one of these options")
-            .setSingleChoiceItems(options, 0) { dialogInterface, i ->
-                Toast.makeText(this, "You clicked on ${options[i]}.", Toast.LENGTH_SHORT).show()
             }
-            .setPositiveButton("Accept") { _, _ ->
-                Toast.makeText(this, "You accepted the SingleChoiceDialog", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("Decline") { _, _ ->
-                Toast.makeText(this, "You Decined the single choice dialog.", Toast.LENGTH_SHORT).show()
-            }.create()
-
-        btnDialog2.setOnClickListener {
-            singleChoiceDialog.show()
-        }
-
-
-        val multiChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one of these options")
-            .setMultiChoiceItems(options, booleanArrayOf(false, false, false)) { _, i, isChecked ->
-                if(isChecked) {
-                    Toast.makeText(this, "You checked ${options[i]}", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "You unchecked ${options[i]}", Toast.LENGTH_SHORT).show()
-                }
-            }
-            .setPositiveButton("Accept") { _, _ ->
-                Toast.makeText(this, "You accepted the MultiChoiceDialog", Toast.LENGTH_SHORT).show()
-            }
-            .setNegativeButton("Decline") { _, _ ->
-                Toast.makeText(this, "You Declined the MultiChoiceDialog.", Toast.LENGTH_SHORT).show()
-            }.create()
-
-        btnDialog3.setOnClickListener {
-            multiChoiceDialog.show()
         }
     }
 
