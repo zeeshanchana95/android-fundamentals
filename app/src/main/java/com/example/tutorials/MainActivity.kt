c4f69b3bbb344081b96ef4bbe957d72ccb49a4f8
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2025-01-07 21:36:42 +0500

    create tab layout in app

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.tutorials
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +31,29 @@ class MainActivity : AppCompatActivity() {
         )
 
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
 
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
         viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
 
-        viewPager.beginFakeDrag()
-        viewPager.fakeDragBy(-10f)
-        viewPager.endFakeDrag()
+        TabLayoutMediator(tabLayout, viewPager) { tab,  position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Unselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+
+            }
+        })
     }
 }
--- app/src/main/java/com/example/tutorials/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/tutorials/ViewPagerAdapter.kt
@@ -10,7 +10,9 @@ class ViewPagerAdapter (
     val images: List<Int>
 ): RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
 
-    inner class ViewPagerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    inner class ViewPagerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val ivImage: ImageView = itemView.findViewById(R.id.ivImage)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view_pager, parent, false)
@@ -19,7 +21,10 @@ class ViewPagerAdapter (
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
         val curImage = images[position]
-        holder.itemView.ivImage.setImageResource(curImage)
+        holder.ivImage.setImageResource(curImage)
+
+        holder.ivImage.setImageResource(curImage)
+//        holder.itemView.ivImage.setImageResource(curImage)
     }
 
     override fun getItemCount(): Int {
